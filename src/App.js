5860ba2f4012bb3f12d095865b40928f992476f3
AyalaEmanuel <129197003+EmanuelAyala13@users.noreply.github.com>
2023-11-26 10:48:22 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProductoPremium from './components/ProductoPremium';
+import Navbar from './components/Navbar';
+import { MenuProvider } from './MenuContext';
+import MoreProducts from './components/MoreProducts';
+import Footer from './components/Footer';
+import ContactPage from './components/ContactPage';
+import AboutUs from './components/AboutUs';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MenuProvider>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <header className="App-header">
+            <h1>Generación Z</h1>
+          </header>
+          <main>
+            <Routes>
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/about-us" element={<AboutUs />} />
+              <Route
+                path="/"
+                element={
+                  <>
+                    <ProductoPremium />
+                    <MoreProducts />
+                  </>
+                }
+              />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </MenuProvider>
   );
 }
 
 export default App;
+
+
+// Querido programador:
+// Cuando escribí este código, sólo Dios y yo
+// sabíamos cómo funcionaba.
+// Ahora, si sólo Dios lo sabe!
+// Así que si está tratando de 'optimizar'
+// esta rutina y fracasa (seguramente),
+// por favor, incremente el siguiente contador
+// como una advertencia para el siguiente colega:
+// total_horas_perdidas_aquí = 189
